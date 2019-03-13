22049f2cab8ffe5e519db0e5081a7374de1a9d7d
Mitul Patel <33424960+mitulpatel18@users.noreply.github.com>
2019-03-13 05:37:03 +0530

    Procider Bug FIxed

--- src/Providers/MSCoreServiceProvider.php
+++ src/Providers/MSCoreServiceProvider.php
@@ -7,7 +7,7 @@
  */
 
 namespace MS\Provider;
-
+define('DS',DIRECTORY_SEPARATOR);
 
 class MSCoreServiceProvider extends \Illuminate\Support\ServiceProvider
 {
@@ -20,17 +20,17 @@ class MSCoreServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
 
-        $this->loadTranslationsFrom(__DIR__ . '/Lang', 'MS');
+        $this->loadTranslationsFrom(__DIR__ . DS.'Lang', 'MS');
 
-        $this->loadViewsFrom(__DIR__ . '/Views', 'MS');
+        $this->loadViewsFrom(__DIR__ . DS.'Views', 'MS');
 
         $this->publishes([
-            __DIR__ . '/Config/MS.php' => base_path('config/MS.php'),
+            __DIR__ . DS'Config'.DS.'MS.php' => base_path('config/MS.php'),
         ], 'MS_config');
 
 
         $this->publishes([
-            __DIR__.'/MSDB' => base_path('MS/DB/Master')
+            __DIR__.DS.'MSDB' => base_path('MS/DB/Master')
         ], 'MS_db');
 
         if(config('MS.default_route_load')){
