2263f2d685e619e8e8a7a7ef917e933f5bdce215
Mitul Patel <33424960+mitulpatel18@users.noreply.github.com>
2019-03-25 04:40:42 +0530

    changes

---
+++ src/Core/Helper/MSForm.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ms
+ * Date: 25-03-2019
+ * Time: 04:35 AM
+ */
+
+namespace MS\Core\Helper;
+
+
+class MSForm
+{
+
+    public $returnHTML =[ ];
+
+
+    public function addModuleForm($namespace=false,$id=false,$perFix=false,$data=false){
+
+        
+
+
+    }
+
+
+    public function view(){
+
+    }
+
+
+}
