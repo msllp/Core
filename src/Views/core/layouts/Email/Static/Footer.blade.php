56b4716e8cfa1a3fc2601faa7f99920c63fcbeda
msllp <48418814+msllp@users.noreply.github.com>
2020-06-06 04:50:20 +0530

    update

--- src/Views/core/layouts/Email/Static/Footer.blade.php
+++
@@ -1,9 +0,0 @@
-<div style="padding: 10px;border-top-width: 1px;border-top-color: rgba(28,28,28,0.1);border-top-style: solid;margin-top: 10px">
-<span class="text-right"> Best Regards,</span><br><br>
-O<sub>3</sub> ERP Support Team<br>
-
-    <span style="font-size: 10px;color: grey">Do not reply to this e-mail because it is unmonitored.</span>
-    <span style="font-size: 10px;color: grey">This automated e-mail was sent by O<sub>3</sub> ERP Cloud Services to {{ (isset($data) && array_key_exists('toEmail',$data)) ?$data['toEmail']:'test@test.com' }}. For general queries: contact us at <span href="maito:help@o3erp.com">help@o3erp.com</span></span><br>
-<span style="font-size: 12px;color: grey">Copyright © O<sub>3</sub> ERP {{date("Y")}}</span>
-<span style="font-size: 10px;color: grey">All rights reserved by <a style="text-decoration: none;font-weight: bold" href="http://www.millionsllp.com">Million Solutions LLP</a></span>
-</div>
--- src/Views/core/layouts/Email/Static/Header.blade.php
+++
@@ -1,6 +0,0 @@
-<div style="display: block;border-bottom-width: 1px;border-bottom-color: rgba(28,28,28,0.1);border-bottom-style: solid;margin-bottom: 10px;padding: 10px;">
-
-
-    <div style="display: block;"> <img  src="http://cdn.millionsllp.com/logo_v1_black.png" style="max-height:45px"></div>
-
-</div>
